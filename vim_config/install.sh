6d3af117d5119cb51130b50ebb270ecff7fafb70
vagrant <vagrant@localhost.localdomain>
2017-04-10 09:24:29 +0100

    fix for windows sh

--- vim_config/install.sh
+++ vim_config/install.sh
@@ -1,31 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 VundleVimRepository='https://github.com/VundleVim/Vundle.vim.git'
-VundleDestDirInLinux='~/.vim'
-VundleDestDirInWindows='~/vimfiles'
+VundleDestDirInLinux=~/.vim
+VundleDestDirInWindows=~/vimfiles
 OS=$(uname -a)
 
 echo '========>Clone VundleVim repository'
 if [[ $OS == Linux* ]]; then
   rm -rf $VundleDestDirInLinux
   git clone $VundleVimRepository $VundleDestDirInLinux/bundle/Vundle.vim
+  vimrc=.vimrc
 else
   rm -rf $VundleDestDirInWindows
   git clone $VundleVimRepository $VundleDestDirInWindows/bundle/Vundle.vim
+  vimrc=_vimrc
 fi
 echo '========================================'
 echo ''
 
 echo '========>Link vimrc file'
-linkVimrcFile
+rm ~/$vimrc
+ln -s $(pwd)/.vimrc ~/$vimrc
 echo '========================================'
 echo ''
 
 echo 'Enter vim and run command :PluginInstall'
 vim
 echo 'Done!'
-
-linkVimrcFile() {
-  rm ~/.vimrc
-  ln -s $(pwd)/.vimrc ~/.vimrc
-}
